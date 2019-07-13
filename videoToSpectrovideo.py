30a7d039a65db0c6abefffa4bb5a22977689798d
Leó Stefánsson <leostefansson@gmail.com>
2019-07-13 17:42:57 +0000

    combing frames into movie, needs refactor.

--- videoToSpectrovideo.py
+++ videoToSpectrovideo.py
@@ -13,7 +13,7 @@ height = 1080
 
 out, _ = (
     ffmpeg
-    .input('input/split_waves_short.mp4')
+    .input('input/nesid_ocean_short.mp4')
     .output('pipe:', format='rawvideo', pix_fmt='gray')
     .run(capture_stdout=True)
 )
@@ -26,6 +26,7 @@ video = (
 print("\n \n ------------------------------------- \n \n")
 print("video.shape (ndarray) --> " + str(video.shape))
 
+# Iterate through all the video frames and save out a png
 for i, frame in enumerate(video):
     print("converting frame to fft")
     f = np.fft.fft2(frame)
@@ -37,9 +38,17 @@ for i, frame in enumerate(video):
     magnitude_spectrum.max()
 
     # Save the image
-    outfile_path = 'output/fft_video_frames/split_wave_short_fft_%s.png'%str(i).zfill(3)
+    outfile_path = 'output/fft_video_frames/nesid_ocean_short_fft_%s.png'%str(i).zfill(3)
     cv2.imwrite(outfile_path, magnitude_spectrum)
 
+
+# Composite the frames into a video
+(
+    ffmpeg
+    .input('output/fft_video_frames/*.png', pattern_type='glob', framerate=60)
+    .output('movie.mp4')
+    .run()
+)
 
 # Using NumPy
 # This saves out a good looking PNG at the end
