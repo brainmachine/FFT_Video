a881f76912dbbdc62ead082a209066ca836dee57
Leó Stefánsson <leostefansson@gmail.com>
2019-07-18 23:30:52 +0000

    refactoring, preparing for multiprocessing

--- videoToSpectrovideo.py
+++ videoToSpectrovideo.py
@@ -1,29 +1,66 @@
-# from PIL import Image
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import ffmpeg
 import os
 
-# Load a video for conversion
-# in_file = ffmpeg.input('input/split_waves.mp4')
 
-width = 1920
-height = 1080
-
+# Define input_filename, extension and input dir. 
 input_filename = 'nesid_ocean_short'
 extension = '.mp4'
-input_dir = 'input'
+input_dir = 'input' # TODO: Make this constant
 
+def convertVideoToImages(video):
+    # Iterate through all the video frames and save out a PNGs
+    for i, frame in enumerate(video):
+        print("converting frame to fft")
+        f = np.fft.fft2(frame)
+        fshift = np.fft.fftshift(f)
+        magnitude_spectrum = 20*np.log(np.abs(fshift))
+        
+        # Normalize magnitude_spectrum
+        magnitude_spectrum = magnitude_spectrum/magnitude_spectrum.max()*255.0
+        magnitude_spectrum.max()
+
+        # Save the image
+        outfile_path = 'output/%s/%s_fft_%s.png'% (input_filename, input_filename, str(i).zfill(3))
+        print(outfile_path)
+        cv2.imwrite(outfile_path, magnitude_spectrum)
+
+
+
+# Splice the path components together5
 input_path = os.path.join(input_dir, (input_filename+extension))
 
+# TODO: populate these with ffprobe
+# TODO: automatically pick the right stream (if there are multiple streams)
+fileinfo = ffmpeg.probe(input_path)
+width = fileinfo['streams'][1]['width']
+height = fileinfo['streams'][1]['height']
+
+# duration = float(fileinfo['format']['duration'])
+# num_frames = int(fileinfo['format']['size'])
+# fps = num_frames/duration
+_, fps = fileinfo['streams'][0]['time_base'].split('/')
+fps = int(fps)
+# left, right = fps.split('/')
+
+# Create output path at output/input_file_name. 
+# This is where we save all the processed FFT PNGs 
+# before we composite them into a new video. 
+image_path = 'output/%s'%input_filename
+if not os.path.exists(image_path):
+    os.mkdir(image_path)
+    print("Created path --> %s"%image_path)
+
+# Load the video
 out, _ = (
     ffmpeg
     .input(input_path)
     .output('pipe:', format='rawvideo', pix_fmt='gray')
     .run(capture_stdout=True)
 )
+# Convert to Numpy Array
 video = (
     np
     .frombuffer(out, np.uint8)
@@ -33,34 +70,19 @@ video = (
 print("\n \n ------------------------------------- \n \n")
 print("video.shape (ndarray) --> " + str(video.shape))
 
-# Iterate through all the video frames and save out a png
-for i, frame in enumerate(video):
-    print("converting frame to fft")
-    f = np.fft.fft2(frame)
-    fshift = np.fft.fftshift(f)
-    magnitude_spectrum = 20*np.log(np.abs(fshift))
-    
-    # Normalize magnitude_spectrum
-    magnitude_spectrum = magnitude_spectrum/magnitude_spectrum.max()*255.0
-    magnitude_spectrum.max()
+convertVideoToImages(video)
 
-    # Save the image
-    outfile_path = 'output/%s/%s_fft_%s.png'% (input_filename, input_filename, str(i).zfill(3))
-    print(outfile_path)
-    cv2.imwrite(outfile_path, magnitude_spectrum)
-
-
-# TODO: Create output path at output/input_file_name
-image_path = 'output/%s'%input_filename
-if not os.path.exists(image_path):
-    os.mkdir(image_path)
-    print("Created path --> %s"%savepath)
 # Composite the frames into a video
+
+# Reac the PNGs from here
 input_path = 'output/%s/*.png'%input_filename
+
+# Save the composite video here
+output_path = '%s_fft.%s'%(input_filename, extension)
 (
     ffmpeg
-    .input(input_path, pattern_type='glob', framerate=60)
-    .output('movie003.mp4')
+    .input(input_path, pattern_type='glob', framerate=fps)
+    .output(output_path)
     .run()
 )
 
