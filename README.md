6cc6ad54ca643228b535d816a35109cd412fdc04
Leó Stefánsson <leostefansson@gmail.com>
2019-09-21 15:59:47 +0000

    refactoring loadVideo() and added README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hidden Sector
+## The World as Waveform
+
+## The conversation
+### How the conversation shaped the work
+- Symmetry 
+- Hidden sector
+- Consciousness 
+
+As a group we engaged with a variety of topics, including symmetry, consciousness, magic and the hidden sector. Early on, it became rather evident that we all had a different understanding of words and concepts and finding points of contact between art and physics was one of the more challenging and rewarding parts of the process. 
+Early on we discussed ideas of symmetry (and symmetry-breaking) in physics - a concept that is both interesting and relatable to visual thinkers.
+
+- [image] (yin/yang)
+    
+## What I have generated for the project (how many pieces)
+
+I'm coming into this project informed by my previous work with particle collision data. 
+
+## Technical requirements and space needed
+
+
--- videoToSpectrovideo.py
+++ videoToSpectrovideo.py
@@ -6,6 +6,7 @@ import os
 import multiprocessing as mp
 mp.set_start_method('spawn', True)
 
+# TODO: BUG: Output videos are significantly git shorter than the input!
 # TODO: Run FFT on RGB channels separately and splice them back together. Should make nice colors that emphasize differnce.
 # TODO: Idea: Stack the output PNGs like pieces of transparent paper
 # TODO: Use a rolling window and look at a segment of the stack evolve. 
@@ -37,14 +38,20 @@ def createExportPath():
         os.mkdir(image_path)
         print("Created path --> %s"%image_path)
 
-def loadVideo():
+def loadVideo(pix_fmt):
     # Load the video
     # TODO: "deprecated pixel format used, make sure you did set range correctly"
     # https://stackoverflow.com/questions/23067722/swscaler-warning-deprecated-pixel-format-used
-    out, _ = (
-        ffmpeg
+    # out, _ = (
+    #     ffmpeg
+    #     .input(input_path)
+    #     .output('pipe:', format='mp4')
+    #     .run(capture_stdout=True)
+    # )
+    out, _  = (
+    ffmpeg
         .input(input_path)
-        .output('pipe:', format='rawvideo', pix_fmt='gray')
+        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
         .run(capture_stdout=True)
     )
     # Convert to Numpy Array
@@ -119,16 +126,16 @@ if __name__ == '__main__':
     height = fileinfo['streams'][1]['height']
 
     fps = getFps()
-
+    pix_fmt = fileinfo['streams'][1]['pix_fmt']
     video = np.empty(shape=[1, 1])
     if doProcess:
         # Load video file
-        loadVideo()
+        loadVideo(pix_fmt)
         # Create output path at output/input_file_name. 
         createExportPath()
 
         
-        video = loadVideo()
+        video = loadVideo(pix_fmt)
 
     print("\n \n ------------------------------------- \n \n")
     print("video.shape (ndarray) --> " + str(video.shape))
